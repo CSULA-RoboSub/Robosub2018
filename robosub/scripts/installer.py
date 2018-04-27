8da1d047933f0b232415ad274186a73aac7991fe
ellipsclamation <cs.henryho@gmail.com>
2018-04-27 16:43:07 -0700

    fixed installer and added logging to file

--- robosub/scripts/installer.py
+++ robosub/scripts/installer.py
@@ -1,8 +1,31 @@
-from scripts import setup_ros
+from scripts.setup_ros import Installer
 
-response = raw_input(
-                '\nAre you sure you want to do first time setup for robosub? [y/n]: '
-            ).lower()
-if response.lower() == 'y':
-    print('Setting up dependencies for Ubuntu 17.04')
-    setup_ros.install()
+
+def main():
+    installer = Installer()
+
+    response = raw_input(
+                    '\nAre you sure you want to do first time setup for robosub? [y/n]: '
+                ).lower()
+    if response == 'y':
+        print('Setting up dependencies for Ubuntu 17.04')
+        installer.install()
+        installer.set_dialout()
+    elif response == 'n':
+        selection = raw_input(
+                    '\nInput the number to install the script:\
+                    \n-1: all\
+                    \n0: fix_ubuntu\
+                    \n1: setup_ros\
+                    \n2: ros_environment\
+                    \n3: ros_serial_setup\
+                    \n4: set_dialout\
+                    \n>> '
+                )
+        if int(selection) >= -1 and int(selection) <= 3:
+            installer.install(selection)
+        elif int(selection) == 4:
+            installer.set_dialout()
+
+if __name__ == "__main__":
+    main()
