335ddd58af0e40ff14948fdb78e94bb75f67fd4f
blah <synergy1095@gmail.com>
2018-07-09 21:50:46 -0700

    small change

--- hardware_interface/src/include/DVLHelper.h
+++ hardware_interface/src/include/DVLHelper.h
@@ -5,8 +5,8 @@
 #include <algorithm>
 // using namespace std;
 
-// static const double PI = 3.1415927;
-const double pi = 3.14159265358979323846;
+// static const float PI = 3.1415927;
+const double PI = 3.14159265358979323846;
 
 class DVLHelper
 {
@@ -18,9 +18,11 @@ class DVLHelper
 		//positive is right negative is left
 		static double getDirection(double x1, double y1, double x2, double y2, double yaw);
 		static double getDistance(double x1, double y1, double x2, double y2);
-		static double getPI(){ return pi; }
-		static double rad2deg(double radTheta){ return (radTheta * (180.0/pi)); }
-		static double deg2rad(double degTheta){ return (degTheta * (pi/180.0)); }
+		//PI getter
+		static double getPI(){ return PI; }
+		//converts radians/degrees
+		static double rad2deg(double radTheta){ return (radTheta * (180.0/PI)); }
+		static double deg2rad(double degTheta){ return (degTheta * (PI/180.0)); }
 };
 
 #endif  // DVLHELPER_H_
