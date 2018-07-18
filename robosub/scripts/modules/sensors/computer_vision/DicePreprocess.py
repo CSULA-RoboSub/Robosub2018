20ccb616c3b250bf3aa29e859909d66f33cd3849
Danny Padilla <dpadilla010@gmail.com>
2018-07-18 12:51:28 -0700

    remove show_preprocess_images method - unused

--- robosub/scripts/modules/sensors/computer_vision/DicePreprocess.py
+++ robosub/scripts/modules/sensors/computer_vision/DicePreprocess.py
@@ -30,9 +30,8 @@ class DicePreprocessor:
         return output, mask, imhsv, imhsv_blur, hsv_filter
 
     def get_interest_regions(self,frame):
-
-
-        height, width, lines = frame.shape
+        
+        height, width, ch = frame.shape
         center = (width / 2, height / 2)
         pimage, mask ,hsv,hsv_blur,hsv_filter = self.preprocess(frame)
         imgray = cv2.cvtColor(pimage, cv2.COLOR_BGR2GRAY)
@@ -47,19 +46,3 @@ class DicePreprocessor:
         interest_regions = [b for b in boxes if b[2]*b[3] > self.roi_size]
 
         return interest_regions
-
-    def show_preprocess_images(self,frame):
-        height, width, lines = frame.shape
-        center = (width / 2, height / 2)
-        pimage,  mask, hsv, hsv_blur, hsv_filter = self.preprocess(frame)
-        imgray = cv2.cvtColor(pimage, cv2.COLOR_BGR2GRAY)
-        flag, binary_image = cv2.threshold(imgray, 85, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        edges = cv2.Canny(binary_image, 50, 150)
-
-        im, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.imshow('pimage',pimage)
-        cv2.imshow('hsv',hsv)
-        cv2.imshow('hsv_blur',hsv_blur)
-        cv2.imshow('hsv_filter',hsv_filter)
-        cv2.imshow('mask',mask)
-
