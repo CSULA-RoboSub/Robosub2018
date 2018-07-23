0932cc3e1186008bb224eb7bbf5f0f83f0cfda27
Danny Padilla <dpadilla010@gmail.com>
2018-07-22 21:51:47 -0700

    change from global to adaptive thresholding

--- robosub/scripts/modules/sensors/computer_vision/DicePreprocess.py
+++ robosub/scripts/modules/sensors/computer_vision/DicePreprocess.py
@@ -9,13 +9,14 @@ class DicePreprocessor:
         self.upper = np.array([255, 255, 93], 'uint8') # olins orig - works for dots
         self.dots_lower = np.array([0, 0, 0], 'uint8') # any lighting
         self.dots_upper = np.array([180, 255, 80], 'uint8') # any lighting
+        self.detect_dots = False
         self.min_cont_size = 100
         self.max_cont_size = 1000
         self.roi_size = 300
-        self.detect_dots = False
-
         self.ratio_lower = 0.85
         self.ratio_upper = 1.15
+
+        
     def preprocess(self, img):
         if (self.detect_dots):
             mask = cv2.inRange(img, self.dots_lower, self.dots_upper)
@@ -41,10 +42,8 @@ class DicePreprocessor:
 
         hsv2bgr_frame = cv2.cvtColor(color_filter_frame, cv2.COLOR_HSV2BGR) # change color space to BGR
         grayscale_frame = cv2.cvtColor(hsv2bgr_frame, cv2.COLOR_BGR2GRAY) # to grayscale
-        
-        flag, thresh_frame = cv2.threshold(grayscale_frame, 100, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #edges = cv2.Canny(binary_image, 50, 150)
 
+        thresh_frame = cv2.adaptiveThreshold(grayscale_frame, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 3)
         frame_c, frame_contours, frame_hierarchy = cv2.findContours(thresh_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         
         boxes = [cv2.boundingRect(c) for c in frame_contours]
