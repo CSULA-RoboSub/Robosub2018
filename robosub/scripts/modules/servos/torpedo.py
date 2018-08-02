79583d17db33769dd8ed740af0b60361dbf126d3
ellipsclamation <ellipsclamation@gmail.com>
2018-08-02 10:21:51 -0700

    fix servo arduino file and update torpedo

--- robosub/scripts/modules/servos/torpedo.py
+++ robosub/scripts/modules/servos/torpedo.py
@@ -5,32 +5,36 @@ from modules.main.status import log
 
 """ Controls torpedoes"""
 
-sides = {
+PRIME_TORPEDO = 0
+FIRE_TORPEDO = 1
+SIDES = {
     'left': 0,
-    'right': 1,
-    'both': 2
+    'right': 1
 }
 
+torpedo = Torpedo()
 pub = rospy.Publisher('torpedo', Torpedo, queue_size=100)
 
 def prime_torpedo(side):
     """
     Prime the torpedo with given side/s
-    side -- (str) left, right, both
+    side -- (str) left, right
     """
 
-    if sides['both'] == sides[side]:
-        print('prime both torpedoes')
-    elif side in sides:
+    if side in SIDES:
+        torpedo.state = PRIME_TORPEDO
+        torpedo.torpedo_number = SIDES[side]
+        pub(torpedo)
         print('prime %s torpedo' % side)
 
 def fire_torpedo(side):
     """
     Fire the torpedo with given side/s
-    side -- (str) left, right, both
+    side -- (str) left, right
     """
 
-    if sides['both'] == sides[side]:
-        print('fire both torpedoes')
-    elif side in sides:
+    if side in SIDES:
+        torpedo.state = FIRE_TORPEDO
+        torpedo.torpedo_number = SIDES[side]
+        pub(torpedo)
         print('fire %s torpedo' % side)
