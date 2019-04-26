47d235b9f40a0ecb8ec69b491e9ab7eb7d518d0a
Jesus <JlopezCera@gmail.com>
2019-04-26 00:22:55 -0700

    New States

--- robosub/scripts/modules/main/new_houston.py
+++ robosub/scripts/modules/main/new_houston.py
@@ -31,7 +31,11 @@ from modules.tasks.task import Task
 
 class Houston():
 
-    states = ['searching','path','buoys','garlic',{'name': 'torpedo','children':['uncover','prime','fired']},'octagon','pingers','gate','finish']
+
+    states = ['searching','path',{'name': 'buoy', 'children' :['searchMonster','triangle','jiangshi','position','ram']},
+            {'name':'garlic', 'children':['findLever','pullLever','foundTarget','findGarlic','pickUpGarlic','drop']},
+            {'name': 'torpedo','children':['lookForLever','uncover','lookForOtherTarget','prevCoveredTarget','position','fire']},
+            {'name' :'octagon', 'children': ['findVampire','pickUpVampire','rise']},'pingers','gate','runCompleted']
     def __init__(self, navigation):
         self.pregate = PreGate(self)
         self.gate = Gate(self)
@@ -40,7 +44,7 @@ class Houston():
         self.cvcontroller = CVController()
         self.counts = Counter()
         self.task_timer = 300
-        self.break_timer = 600    
+        self.break_timer = 600
 
         ################ AUV MOBILITY VARIABLES ################
         # self.rotational_movement = {-1: }
@@ -53,31 +57,61 @@ class Houston():
         self.is_killswitch_on = False
 
         self.last_time = time.time()
-        m = Houston()
-        machine = Machine(model=m, states=states, initial='searching')
-
 
         transitions = [
-        ['goToGateTask','searching','gate'],
-        ['completeGateTask','gate','=','gateTaskNotCompleted'],
-        ['completeGateTask','gate','path','gateTaskCompleted'],
-        ['pathToBuoyTask','path','buoys','atAssignedAngle'],
-        ['pathToBuoyTask','path','=','notAtAssignedAngle'],
-        ['completeBuoyTask','buoys','=','buoysTaskNotCompleted'],
-        ['completeBuoyTask','buoys','path','buoysTaskCompleted'],
-        ['pathToTorpedoTask','path','torpedo_uncover','atAssignedAngle'],
-        ['uncoverBox','torpedo_uncover','torpedo_prime', 'targetUncovered'],
-        ['uncoverBox','torpedo_uncover','=','targetCovered'],
-        ['primeTorpedo','torpedo_prime','=','torpedoNotPrimedCorrectly'],
-        ['fireTorpedo','torpedo_prime','torpedo_fired','torpedoPrimedCorrectly'],
-        ['completeGarlicTask','garlic','=','garlicTaskNotCompleted'],
-        ['pingerToOctagon','pingers','octagon'],
-        ['pingerToGarlic','pingers','garlic'],
-        ['usePingersFromTorpedo','torpedo_fired','pingers','torpedoFired'],
-        ['usePingersFromGarlic','garlic','pingers','garlicTaskCompleted'],
-        ['octagontoSurface','octagon','finish','octagonTaskCompleted'],
-        ['completeOctagonTask','octagon','=']
+         ['completeGateTask','gate','=','gateTaskNotCompleted'],
+         ['completeGateTask','gate','path','gateTaskCompleted'],
+         ['pathToNextTask','path','searching','atAssignedAngle'],
+         ['pathToNextTask','path','=','notAtAssignedAngle'],
+         ['identifyBuoyMonster','buoy_searchMonster','buoy_triangle','monsterFound'],
+         ['identifyBuoyMonster','buoy_searchMonster','buoy_jiangshi','monsterNotFound'],
+         ['findMonsterPosition','buoy_triangle','buoy_position','subInPosition'],
+         ['findMonsterPosition','buoy_jiangshi','buoy_position','subInPosition'],
+         ['findMonsterPosition','buoy_triangle','buoy_jiangshi','tooManyPositionAttempts'],
+         ['findMonsterPosition','buoy_jiangshi','searching','tooManyPositionAttempts'],
+         ['ramMonster','buoy_position','buoy_ram'],
+         ['buoyTaskCompleted','buoy_ram','searching'],
+         ['findLever','torpedo_lookForLever','torpedo_uncover','leverFound'],
+         ['findLever','torpedo_lookForLever','torpedo_lookForOtherTarget','leverNotFound'],
+         ['findLever','torpedo_lookForLever','torpedo_fire',['leverNotFound','targetNotFound']],
+         ['pullLevel','torpedo_uncover','torpedo_prevCoveredTarget','leverPulled'],
+         ['pullLevel','torpedo_uncover','torpedo_lookForOtherTarget','leverNotPulled'],
+         ['findTargetPosition','torpedo_prevCoveredTarget','torpedo_position','foundTarget'],
+         ['findTargetPosition','torpedo_lookForOtherTarget','torpedo_position','foundTarget'],
+         ['findTargetPosition','torpedo_prevCoveredTarget','torpedo_fire','targetNotFound'],
+         ['findTargetPosition','torpedo_lookForOtherTarget','torpedo_fire','targetNotFound'],
+         ['readytoFire','torpedo_position','torpedo_fire',['subInPosition','notTooManypositioningAttempts']],
+         ['readytoFire','torpedo_position','torpedo_fire','tooManypositioningAttempts'],
+         ['torpedoFired','torpedo_fire','searching','torpedoWasFired'],
+         ['garlicFindLever','garlic_findLever','garlic_pullLever','leverFound'],
+         ['garlicFindLever','garlic_findLever','garlic_findGarlic','leverNotFound'],
+         ['garlicPullLever','garlic_pullLever','garlic_findGarlic','leverPulled'],
+         ['garlicPullLever','garlic_pullLever','=',['leverNotPulled','notTooManyAttempts']],
+         ['garlicPullLever','garlic_pullLever','garlic_findGarlic','tooManyAttempts'],
+         ['garlicFindGarlic','garlic_findGarlic','garlic_pickUpGarlic','garlicFound'],
+         ['garlicFindGarlic','garlic_findGarlic','=','garlicNotFound'],
+         ['garlicFindTarget','garlic_pickUpGarlic','garlic_foundTarget','targetFound'],
+         ['garlicFindTarget','garlic_pickUpGarlic','=',['targetNotFound','notTooManyAttempts']],
+         ['garlicPositionOverTarget','garlic_foundTarget','garlic_drop','maintainingPosition'],
+         ['garlicDropped','garlic_drop','searching','garlicReleased'],
+         ['octagonSearch','octagon_findVampire','octagon_pickUpVampire','vampireFound'],
+         ['octagonSearch','octagon_findVampire','=','vampireNotFound'],
+         ['vampirePickUp','octagon_pickUpVampire','octagon_rise','vampirePickedUp'],
+         ['vampirePickUp','octagon_pickUpVampire','=','vampireNotPickedUp'],
+         ['octagonToSurface','octagon_rise','runCompleted'],
+         ['searchForTask',['garlic_drop','torpedo_fire','pingers','buoy_ram','buoy_jiangshi'],'searching'],
+         ['searchForPath','searching','path','pathNumberUnderTwo'],
+         ['searchForBuoy','searching','buoy_searchMonster','buoysTaskNotCompleted'],
+         ['searchForPinger','searching','pingers','pathNumberOverTwo'],
+         ['searchForTorpedo','searching','torpedo_lookForLever','torpedoTaskNotCompleted'],
+         ['searchForGarlic','searching','garlic_findLever','garlicTaskNotCompleted'],
+         ['searchForOctagon','searching','octagon_findVampire','octagonTaskNotCompleted']
         ]
+
+        m = Houston()
+        machine = GraphMachine(model=m, states=states,transitions=transitions,initial='gate')
+
+
 
     def gateTaskNotCompleted(self):
         return True
